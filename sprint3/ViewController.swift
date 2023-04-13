12cba9cce5012bdeb275e362e520a4151a2fecd2
Diana <dianabalahnova@yandex.ru>
2023-04-13 11:51:38 +0300

    задача третьего спринта.

--- sprint3/ViewController.swift
+++ sprint3/ViewController.swift
@@ -10,7 +10,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var scoreLabel: UILabel!
-    @IBOutlet weak var tapButton: UIButton!
+    
+    @IBOutlet weak var plusButtonScore: UIButton!
+    
+    @IBOutlet weak var minusButtonScore: UIButton!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBOutlet weak var changingHistory: UITextView!
+    
+    @IBOutlet weak var ScrollForChangingHistory: UIScrollView!
+    
     
     var score = 0 {
         didSet {
@@ -18,20 +28,70 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func buttonTap(_ sender: Any) {
+    var now = Date()
+
+    var dateFormeter = DateFormatter()
+    
+    @IBAction func plusScore(_ sender: Any) {
         score += 1
+        now = Date()
+        changingHistory.text += "\(dateFormeter.string(from: now)) значение изменено на +1 \n"
+        sizeMatchingTextAndScroll()
     }
+    
+    @IBAction func minusScore(_ sender: Any) { if score > 0 {score -= 1
+        now = Date()
+        changingHistory.text += "\(dateFormeter.string(from: now)) значение изменено на -1 \n"
+        sizeMatchingTextAndScroll()
+    } else {
+        score = 0
+        now = Date()
+        changingHistory.text += "\(dateFormeter.string(from: now)) попытка уменьшить значение счётчика ниже 0 \n"
+       sizeMatchingTextAndScroll()
+    }
+       
+    }
+    
+    
+    @IBAction func resetScore(_ sender: Any) { if score > 0 {
+        score = 0
+        now = Date()
+      sizeMatchingTextAndScroll()
+        
+    }
+        now = Date()
+        changingHistory.text += "\(dateFormeter.string(from: now)) значение сброшено \n"
+        sizeMatchingTextAndScroll()
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         scoreLabel.text = "Значение счетчика: 0"
-    
+        changingHistory.text = "История изменений: \n"
+        
+        dateFormeter.timeStyle = .short
+        dateFormeter.dateStyle = .short
+        
+        ScrollForChangingHistory.addSubview(changingHistory)
+        
+        ScrollForChangingHistory.contentSize = self.changingHistory.bounds.size
+        
     }
     
     func updateScoreLabel () {
         scoreLabel.text = "Значение счетчика: \(score)"
+        
     }
+    
+    func sizeMatchingTextAndScroll() {
+        changingHistory.sizeToFit()
+        ScrollForChangingHistory.contentSize = self.changingHistory.bounds.size
+    }
+    
+   }
 
-}
+
+
 
